4ff3d7d3492c5275580a3ad0bfb0bf889fb6ba12
Анатолий Боровик <anatoly.borovik95@gmail.com>
2026-02-03 12:43:41 +0200

    Class TaskItem better data types and fields version

---
+++ src/TimeManager.Core/TaskItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Core
+{
+    public class TaskItem
+    {
+        private string _name
+        {
+            get; set;
+        }
+        private string _description
+        {
+            get; set;
+        }
+        private byte _category
+        {
+            get; set;
+        }
+        //Have to add special structer, where 0 is None Category,
+        //user will have acces to add new categories
+        private DateTime _deadLine
+        {
+            get; set;
+        }
+        private TimeSpan _timeToDo
+        {
+            get; set;
+        }
+        private byte _priority //1 - 10
+        {
+            get; set;
+        }
+        private byte _dificulty //1 - 100
+        {
+            get; set;
+        }
+
+        private bool _isItDone;
+        public TaskItem(
+            string Name, string Description, byte Category,
+            DateTime DeadLine, TimeSpan TimeToDo,
+            byte priority = 5, byte dificulty = 20,
+            bool IsItDone = false)
+        {
+            _name = Name;
+            _description = Description;
+            _category = Category;
+            _priority = priority;
+            _isItDone = IsItDone;
+            _deadLine = DeadLine;
+            _timeToDo = TimeToDo;
+        }
+        public void MarkDone()
+        {
+            _isItDone = true;
+        }
+    }
+}
--- src/TimeManager.Core/obj/Debug/net9.0/TimeManager.Core.AssemblyInfo.cs
+++ src/TimeManager.Core/obj/Debug/net9.0/TimeManager.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeManager.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f93433e129d74fc9126e9172bd24aba89de8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584274140a595da8a111b88849e870f0f525068f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeManager.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeManager.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
