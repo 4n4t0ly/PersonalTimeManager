5d458c03eff97fb75977057e0d43731b9571dffe
Анатолий Боровик <anatoly.borovik95@gmail.com>
2026-02-06 21:31:58 +0200

    Added exeptions to TaskItem class

--- src/TimeManager.Core/TaskItem.cs
+++ src/TimeManager.Core/TaskItem.cs
@@ -19,20 +19,23 @@ namespace TimeManager.Core
         public byte Priority { get; private set; } //1 - 10
         public byte Dificulty { get; private set; } //1 - 100
 
-        public bool _isDone { get; private set; }
+        public bool IsDone { get; private set; }
         public TaskItem(
-            string name, string Description, string Category,
-            DateTime DeadLine, TimeSpan TimeToDo,
+            string name, string description, string category,
             byte priority = 5, byte dificulty = 20)
         {
             Name = name;
-            Description = Description;
-            Category = Category;
+            Description = description;
+            Category = category;
             Priority = priority;
         }
         public void MarkDone()
         {
-            _isDone = true;
+            IsDone = true;
+        }
+        public void SetPriority(byte priority)
+        {
+
         }
     }
 }
