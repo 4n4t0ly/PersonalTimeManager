3bdd39937c4bbcb9ac800db904f696860609cbbf
Анатолий Боровик <anatoly.borovik95@gmail.com>
2026-06-01 19:16:55 +0300

    Firts try to make unit tests

---
+++ tests/TimeManager.Tests/TaskItemTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TimeManager.Tests
+{
+    [TestClass]
+    public class PTM_Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
